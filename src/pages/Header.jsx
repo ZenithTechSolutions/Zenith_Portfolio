b2585dbfc535c295a1a2b1126fdfeff9a0eb1dac
VISHALKANNAN070 <vishalkannan070@gmail.com>
2025-05-12 22:19:42 +0530

    working on nav

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -4,13 +4,21 @@ import logo from '../assets/Z-bold-no-bg.png'
 function Header() {
   return (
     <header className='header'>
+      <nav className='navbar-nav'>
       <img src={logo} alt="Zenith Logo" />
-      <nav className='navbar'>
-        <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#project">Projects</a></li>
-          <li><a href="#contact">Contact</a></li>
+        <ul className='navbar-menu'>
+          <li className='nav-item'><a href="#home">Home</a></li>
+          <li className='nav-item'><a href="#about">About</a></li>
+          <li className='nav-item'><a href="#project">Projects</a></li>
+          <li className='nav-item'><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+      <nav className='navbar-ham'>
+        <ul className='hamburger-menu'>
+          <li className='hamburger-item'><a href="#home">Home</a></li>
+          <li className='hamburger-item'><a href="#about">About</a></li>
+          <li className='hamburger-item'><a href="#project">Projects</a></li>
+          <li className='hamburger-item'><a href="#contact">Contact</a></li>
         </ul>
       </nav>
     </header>
