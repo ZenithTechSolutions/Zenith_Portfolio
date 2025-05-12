7cb258405a518f4b26468b02302b93f8c8a128c9
VISHALKANNAN070 <vishalkannan070@gmail.com>
2025-05-12 22:00:20 +0530

    project content

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import '../styles/project.css';
+import React from "react";
+import "../styles/project.css";
 
-const project = [{
-  title: "Poshan Tracker", description: "A diet and nutrition tracker website designed for children in Anganwadis. It helps monitor nutritional intake, track progress, and provide insights for better health planning.", img: null
-}, {
-  title: "SPARK'25", description: "An international technical conference organized in collaboration with IEEE, bringing together researchers, engineers, and students to discuss groundbreaking innovations and advancements in technology.", img: null
-}, {
-  title: "VishvashUniforms", description: "Uniforms Manufacturing and Sales.", img: null
-}]
+const project = [
+  {
+    title: "Poshan Tracker",
+    description:
+      "A nutrition and health monitoring platform designed for children in Anganwadis. It tracks dietary intake, analyzes growth trends, and supports informed planning for improved well-being.",
+  },
+  {
+    title: "SPARK'25",
+    description:
+      "An international tech symposium conducted with IEEE. It brings together experts, researchers, and students to showcase innovations and foster collaborative advancements in technology.",
+  },
+  {
+    title: "Vishvash Uniforms",
+    description:
+      "A uniform manufacturing and retail solution offering customized, high-quality apparel. It serves schools, institutions, and businesses with efficient supply and design services.",
+  },
+];
+
 function Project() {
   return (
     <section id="project">
       <div className="project-container">
         <h1>Projects</h1>
         <div className="project-grid">
-            {project.map((project,index)=>(
-          <div className="project-card"  key={index}>
-            <h2>{project.title}</h2>
-            <p>{project.description}</p>
-          </div>
-      ))}
-      </div>
+          {project.map((project, index) => (
+            <div className="project-card" key={index}>
+              <h2>{project.title}</h2>
+              <p>{project.description}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
